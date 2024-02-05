6bc13ccefc5ab6709adb2d03e1b53efe408bea9e
Ben Hatsor <34835685+benhatsor@users.noreply.github.com>
2024-02-05 14:11:21 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,27 @@
 
-
+const input = document.querySelector('.main-text');
+
+
+input.addEventListener('paste', openURL);
+
+input.addEventListener('keyup', ({key}) => {
+  
+  if (key === 'Enter') openURL();
+    
+});
+
+
+function openURL() {
+  
+  let url = 'https://www.youtube.com/playlist?list=';
+  
+  const channelId = input.textContent.trim();
+  
+  const playlistId = channelId.slice(0, 1) + 'U' + channelId.slice(2);
+  
+  url += playlistId;
+  
+  window.location.href = url;
+  
+}
+
